ae68082fed40265f385f90b98a6f39b29b0123a9
Johannes Brummer <johannes.brummer@upcloud.com>
2021-08-10 17:28:04 +0300

    python3 compatibility, dynamic filelist

--- make-drac-great-again.py
+++ make-drac-great-again.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 '''Requires firmware upgrade files to be available under <chassis>/<file> -structure'''
 '''v1 & v2 pre 2.21.21.21: max path length 64 characters'''
-import sys, getopt
+import sys, getopt, os
 import getpass
 # pexpect is needed for logging interactively(hide password from command line) into a DRAC
 import pexpect
@@ -10,48 +10,24 @@ from time import sleep
 chassis_supported = ['R620','R720xd','R630','R730xd','R640','R6525']
 drac_user = 'root'
 
-R620_DRAC_versions = [
-    ['2.65.65.65','iDRAC-with-Lifecycle-Controller_Firmware_0GHF4_WN64_2.65.65.65_A00.EXE'],
-    ['2.41.40.40','iDRAC-with-Lifecycle-Controller_Firmware_XTPX4_WN64_2.41.40.40_A00.EXE'],
-    ['2.30.30.30','iDRAC-with-Lifecycle-Controller_Firmware_JHF76_WN64_2.30.30.30_A00.EXE'],
-    ['2.21.21.21','iDRAC-with-LCC_VV01T_WN64_2.21.21.21_A00.EXE'],
-    ['2.10.10.10','iDRAC-with-LCC_Y5K20_WN32_2.10.10.10_A00.EXE'],
-    ['1.66.65','ESM_Firmware_3F4WV_WN64_1.66.65_A00.EXE']
-    ]
-R720xd_DRAC_versions = [
-    ['2.65.65.65','iDRAC-with-Lifecycle-Controller_Firmware_0GHF4_WN64_2.65.65.65_A00.EXE'],
-    ['2.41.40.40','iDRAC-with-Lifecycle-Controller_Firmware_XTPX4_WN64_2.41.40.40_A00.EXE'],
-    ['2.30.30.30','iDRAC-with-Lifecycle-Controller_Firmware_JHF76_WN64_2.30.30.30_A00.EXE'],
-    ['2.21.21.21','iDRAC-with-LCC_VV01T_WN64_2.21.21.21_A00.EXE'],
-    ['2.10.10.10','iDRAC-with-LCC_Y5K20_WN32_2.10.10.10_A00.EXE'],
-    ['1.66.65','ESM_Firmware_3F4WV_WN64_1.66.65_A00.EXE']
-    ]
-R630_DRAC_versions = [
-    ['2.75.100.75','iDRAC-with-Lifecycle-Controller_Firmware_DPV0R_WN64_2.75.100.75_A00.EXE'],
-    ['2.63.60.61','iDRAC-with-Lifecycle-Controller_Firmware_40T1C_WN32_2.63.60.61_A00.EXE'],
-    ['2.41.40.40','iDRAC-with-Lifecycle-Controller_Firmware_4950Y_WN32_2.41.40.40_A00.EXE'],
-    ['2.30.30.30','iDRAC-with-Lifecycle-Controller_Firmware_5GCHC_WN32_2.30.30.30_A00.EXE'],
-    ['2.21.21.21','iDRAC-with-LCC_1X82C_WN64_2.21.21.21_A00.EXE'],
-    ['2.10.10.10','iDRAC-with-LCC_FM1PC_WN64_2.10.10.10_A00.EXE']
-    ]
-R730xd_DRAC_versions = [
-    ['2.75.100.75','iDRAC-with-Lifecycle-Controller_Firmware_DPV0R_WN64_2.75.100.75_A00.EXE'],
-    ['2.63.60.61','iDRAC-with-Lifecycle-Controller_Firmware_40T1C_WN32_2.63.60.61_A00.EXE'],
-    ['2.41.40.40','iDRAC-with-Lifecycle-Controller_Firmware_4950Y_WN32_2.41.40.40_A00.EXE'],
-    ['2.30.30.30','iDRAC-with-Lifecycle-Controller_Firmware_5GCHC_WN32_2.30.30.30_A00.EXE'],
-    ['2.21.21.21','iDRAC-with-LCC_1X82C_WN64_2.21.21.21_A00.EXE'],
-    ['2.10.10.10','iDRAC-with-LCC_FM1PC_WN64_2.10.10.10_A00.EXE']
-    ]
-R640_DRAC_versions = [
-    ['4.40.00.00','iDRAC-with-Lifecycle-Controller_Firmware_62GW1_WN64_4.40.00.00_A00.EXE'],
-    ['3.36.36.36','iDRAC-with-Lifecycle-Controller_Firmware_HFMCV_WN64_3.36.36.36_A00.EXE'],
-    ['3.21.21.21','iDRAC-with-Lifecycle-Controller_Firmware_387FW_WN64_3.21.21.21_A00.EXE']
-    ]
-
-R6525_DRAC_versions = [
-    ['4.40.00.00','iDRAC-with-Lifecycle-Controller_Firmware_62GW1_WN64_4.40.00.00_A00.EXE'],
-    ['3.36.36.36','iDRAC-with-Lifecycle-Controller_Firmware_HFMCV_WN64_3.36.36.36_A00.EXE']
-    ]
+for chassis in chassis_supported:
+    globals()[chassis + '_DRAC_versions'] = []
+    files = os.listdir(chassis)
+    updates = []
+    for file in files:
+        if 'iDRAC' in file:
+            updates.append(file)
+    
+    for update in updates:
+        data = []
+        haystack = update.removesuffix('.EXE').split("_")
+        for needle in haystack:
+            if needle[0].isdigit():
+                if '.' in needle:
+                    data.append(needle)
+                    data.append(update)
+        globals()[chassis + '_DRAC_versions'].append(data)
+    globals()[chassis + '_DRAC_versions'].sort()
 
 def drac_sysinfo_update(ip, drac_user, drac_pass):
     child = pexpect.spawn('/opt/dell/srvadmin/bin/idracadm7 -r ' + ip + ' --nocertwarn -i getsysinfo', timeout=300)
@@ -93,12 +69,12 @@ def drac_check_next_upgrade(drac_list, drac_version):
     drac_upgrade_version=''
     drac_list.sort()
     
-    if drac_major == '1': # Hardcoded upgrade path for v1 -> v2
+    if '20' in chassis and drac_major == '1': # Hardcoded upgrade path for v1 -> v2 (for 12G)
         if drac_minor >= '66':
             drac_upgrade_version = 'iDRAC-with-LCC_Y5K20_WN32_2.10.10.10_A00.EXE'
         else:
             drac_upgrade_version = 'ESM_Firmware_3F4WV_WN64_1.66.65_A00.EXE'
-    elif drac_major in ['2', '3']:
+    elif drac_major >= '1':
         for i in drac_list:
             print('Check upgrade to ' + i[0])
             if is_superior_version(i[0], drac_version):
@@ -148,7 +124,7 @@ def main():
         drac_pass = getpass.getpass(prompt='DRAC password:')
     
     while True:
-        sysinfo = drac_sysinfo_update(drac_ip, drac_user, drac_pass)
+        sysinfo = drac_sysinfo_update(drac_ip, drac_user, drac_pass).decode()
         bios_version = [line for line in sysinfo.split('\n') if "System BIOS Version" in line][-1].split()[-1]
         chassis = [line for line in sysinfo.split('\n') if "System Model" in line][-1].split()[-1]
         drac_version = [line for line in sysinfo.split('\n') if "Firmware Version" in line][-1].split()[-1]
